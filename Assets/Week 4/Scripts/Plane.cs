091e9c68cc2ac81ac0b9d1cc4663e1ab026528cd
rosschas <rosschas@sheridancollege.ca>
2024-02-01 10:25:58 -0500

    Plane is moving. line is updating.

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -8,12 +8,47 @@ public class Plane : MonoBehaviour
     public float newPosThresh = 0.2f;
     Vector2 lastPos;
     LineRenderer lineR;
+    Vector2 currentPos;
+    Rigidbody2D rb;
+    public float speed = 1;
 
     private void Start()
     {
         lineR = GetComponent<LineRenderer>();
         lineR.positionCount = 1;
         lineR.SetPosition(0, transform.position);
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentPos = transform.position;
+        if(points.Count > 0 ) 
+        {
+            Vector2 direction = points[0] - currentPos;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb.rotation = -angle;
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineR.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPos, points[0]) < newPosThresh) 
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineR.positionCount - 2; i++)
+                {
+                    lineR.SetPosition(i, lineR.GetPosition(i + 1));
+                }
+                lineR.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
