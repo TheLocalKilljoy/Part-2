4d8533c6978c2ec10351a03ebc698a921c2ceeca
rosschas <rosschas@sheridancollege.ca>
2024-02-01 10:50:51 -0500

    Plane landing. Task 2 complete

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -11,6 +11,8 @@ public class Plane : MonoBehaviour
     Vector2 currentPos;
     Rigidbody2D rb;
     public float speed = 1;
+    public AnimationCurve landing;
+    float landTimer;
 
     private void Start()
     {
@@ -35,6 +37,17 @@ public class Plane : MonoBehaviour
 
     private void Update()
     {
+        if(Input.GetKey(KeyCode.Space)) 
+        {
+            landTimer += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landTimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+
         lineR.SetPosition(0, transform.position);
         if (points.Count > 0)
         {
