6220319edd2e6d73ecc8215b440de01fa3946a84
Chase <rosschas@sheridancollege.ca>
2024-02-20 23:22:51 -0500

    Made player move using point and click

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+
+    Vector2 end;
+    Vector2 move;
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        move = end - (Vector2)transform.position;
+        if (move.magnitude < 0.5)
+        {
+            move = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + move.normalized * speed * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
     }
 }
